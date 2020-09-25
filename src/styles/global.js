31f7c24e15c426b2fabf50876cabe332e4b55854
Victor Cordeiro <vccode2020@gmail.com>
2020-09-25 16:38:22 +0100

    small style fix

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,8 +2,8 @@ import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
   * {
-    margin: 0,
-    padding: 0,
-    box-sizing: border-box
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
 `

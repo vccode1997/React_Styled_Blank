9fd6dbef9fa7e06bc51ce326cacadfe754b22f5f
Victor Cordeiro <vccode2020@gmail.com>
2020-09-25 14:57:04 +0100

    initial setup eslint + prettier + styled components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import GlobalStyles from './styles/global'
+import { Main } from './styles'
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Main />
+    </>
+  )
+}
+
+export default App
